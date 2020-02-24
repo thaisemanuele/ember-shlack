e4259917f8869540f2f1927a8ff19098d9a16275
thaisemanuele <thatitis@gmail.com>
2020-02-24 11:38:15 +0200

    sending and deleting messages

--- app/components/chat-container.js
+++ app/components/chat-container.js
@@ -1,11 +1,18 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import fetch from 'fetch';
+import AuthService from 'shlack/services/auth';
 
 export default class ChatContainerComponent extends Component {
     @tracked
     messages = [];
+
+    /**
+     * @type {AuthService}
+     */
+    @service auth;
 
     @action
     async loadMessages() {
@@ -13,5 +20,43 @@ export default class ChatContainerComponent extends Component {
         
         const response = await fetch(`/api/teams/${teamId}/channels/${id}/messages`);
         this.messages = await response.json();
+    }
+
+    @action
+    async deleteMessage(messageId) {
+        const response = await fetch(`/api/messages/${messageId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+        const updatedMessages = this.messages.filter(msg => msg.id !== messageId);
+        this.messages = updatedMessages;
+    }
+
+    @action
+    async createMessage(body) {
+        const { channel : {id: channelId, teamId}} = this.args;
+
+        const userId = this.auth.currentUserId;
+        const response = await fetch('/api/messages', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                teamId,
+                channelId,
+                userId,
+                body,
+            })
+        });
+        if (!response.ok) throw Error('Could not send message');
+        const messageData = await response.json();
+        const user = await (await fetch(`/api/users/${userId}`)).json();
+        this.messages = [
+            ...this.messages,
+            { ...messageData, user }
+        ]
     }
 }
